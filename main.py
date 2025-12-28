6baf9f10716129e7ad7c61311677a3f77073203f
Adarsh2004ku <adarsh2022ji@gmail.com>
2025-12-29 02:53:39 +0530

    Data validation Completed

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logger
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
+from networksecurity.entity.artifact_entity import DataIngestionArtifact
+from networksecurity.entity.artifact_entity import DataValidationArtifact
 
 
 
@@ -14,7 +17,16 @@ if __name__=='__main__':
         data_ingestion = DataIngestion(data_ingestion_config)
         logger.info("Initiate the data Ingestion")
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
-        print(dataingestionartifact)
+        logger.info("Completed the data Ingestion")
+        data_validation_config = DataValidationConfig(training_pipeline_config)
+        data_validation = DataValidation(dataingestionartifact, data_validation_config)
+        logger.info("Initiate the data Validation")
+        data_validation.initiate_data_validation()
+        logger.info("Completed the data Validation")
+
+
+
+
 
 
     except Exception as e:
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -23,3 +23,15 @@ FILE_NAME:str = "phishing.csv"
 
 TRAIN_FILE_NAME :str = "train.csv"
 TEST_SPLIT_NAME : str = "test.csv"
+
+
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+
+SCHEMA_FILE_PATH = os.path.join("data_schema","schema.yaml")
+
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
