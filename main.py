578ba39554d042004f674bd911b1016b0c173820
Adarsh2004ku <adarsh2022ji@gmail.com>
2026-01-03 00:57:21 +0530

    data transformation completed

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logger
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 from networksecurity.entity.artifact_entity import DataIngestionArtifact
 from networksecurity.entity.artifact_entity import DataValidationArtifact
+from networksecurity.entity.artifact_entity import DataTransformationArtifact
+from networksecurity.components.data_transformation import DataTransformation
+
 
 
 
@@ -21,9 +24,14 @@ if __name__=='__main__':
         data_validation_config = DataValidationConfig(training_pipeline_config)
         data_validation = DataValidation(dataingestionartifact, data_validation_config)
         logger.info("Initiate the data Validation")
-        data_validation.initiate_data_validation()
+        data_validation_artifact = data_validation.initiate_data_validation()
         logger.info("Completed the data Validation")
-
+        data_transformation_config=DataTransformationConfig(training_pipeline_config)
+        logger.info("data Transformation started")
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logger.info("data Transformation completed")
 
 
 
